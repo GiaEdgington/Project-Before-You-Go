f1636393433deed1da2edcfd2e256819a2906f3b
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-10-07 13:50:01 -0400

    css styling added

--- src/Intro.js
+++ src/Intro.js
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom';
 class Intro extends React.Component {
     render(){
         return(
-            <div>
-                <Link to='./login' className="loginIntro">Login here</Link>
-                <div className="intro"></div>
+            <div className="intro">
+                <div>
+                <Link to='./login' className="loginIntro">Sign in</Link>
+                </div>
+                
             </div>
         )
     }
