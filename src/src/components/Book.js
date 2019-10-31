ea2d2ac1829e395c4153b73547f828c7a57c6ac2
Gia Edgington <gia.edgington@gmail.com>
2019-10-30 22:18:27 -0400

    Add files via upload

---
+++ src/src/components/Book.js
@@ -0,0 +1,106 @@
+import React from 'react';
+
+class Book extends React.Component {
+    state = {
+        title: "",
+        authors: [],
+        image: "",
+        synopsis: "",
+        isHidden: true,
+        added: false,
+        destination: ''
+    }
+
+    componentDidMount() {
+        this.fetchBooks();
+    }
+
+    //fetch for book info, update state
+    fetchBooks = () => {
+        const key2 = 'AIzaSyCUZDVxJS93fWmpk3QKfscn15qz7segx-4';
+        const key1 = 'AIzaSyCH0tIhWJCGZf1HFjw_hRFlJ0vlNuLVtf8';
+
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=+title:${this.props.book}&maxResults=1&key=${key1}`)
+        .then(response => response.json())
+        .then(response => {
+            let title = typeof response.items[0].volumeInfo.title == "undefined" ? "" : response.items[0].volumeInfo.title;
+            let authors = typeof response.items[0].volumeInfo.authors == "undefined" ? "" : response.items[0].volumeInfo.authors;
+            let image = typeof response.items[0].volumeInfo.imageLinks == "undefined" ? "" : response.items[0].volumeInfo.imageLinks.smallThumbnail;
+            let synopsis = typeof response.items[0].volumeInfo.description == "undefined" ? "Information not available." : response.items[0].volumeInfo.description;
+
+            this.setState({ title: title,
+                            authors: authors,
+                            image: image,
+                            synopsis: synopsis,
+                            finished: true
+                         });
+        });
+    };
+
+    //for book details
+    handleClick = () => {
+        this.setState({ isHidden: !this.state.isHidden})
+    };
+
+    //saving book
+    //find if destination already exists
+    addBook = () => {
+
+        fetch('https://before-you-go.herokuapp.com/books', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accepts': 'application/json'
+            },
+            body: JSON.stringify({
+                title: this.state.title,
+                author: this.state.authors.join(),
+                image: this.state.image,
+                synopsis: this.state.synopsis,
+                destination_id: this.props.destination_id
+            })
+        }).then(response => response.json())
+        .then(() => {
+            //should look at response and confirm trip was added. this is temporary
+            this.setState({added: true})
+        });  
+
+    };
+
+    render(){
+        return (
+            <div className="flex-item" >
+                { this.state.image == "" 
+                ?
+                 <div style={{ width:'130px',height:'200px',border:'1px black solid',backgroundColor:'white'}}>
+                     <p style={{ padding:'1em', fontWeight:'bolder', fontSize:'1em'}}>{this.state.title}</p>
+                </div>
+                 :
+                 <img src={this.state.image} style={{ width:'130px'}} alt="" />
+                }
+                <button onClick={this.handleClick} className="buttonPage">Learn more</button>
+                { this.props.destination_id && !this.state.added
+                ?
+                <button className="buttonPage" onClick={this.addBook}>Add Book</button>
+                :
+                <div>
+                    { this.state.added
+                    ?
+                    <p>Book has been added!</p>
+                    :
+                    <div></div>
+                    }
+                </div>
+                }
+                { !this.state.isHidden && this.state.synopsis != ""
+                ? 
+                <div className="synopsis"><h4>{this.state.title}</h4><p>{this.state.synopsis}</p></div>
+                :
+                <p></p>
+                }
+            </div>
+        )
+    }
+}
+
+export default Book;
---
+++ src/src/components/Form.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import BookDisplay from '../containers/BookDisplay';
+
+class Form extends React.Component {
+
+    destinationSearch = ""
+
+    state = {
+        destination: "Your destination",
+        destination_id: null,
+        books:[],
+        message: "",
+        notFound: null
+    }
+
+    handleChange = (destination) => {
+        this.destinationSearch = destination;
+    }
+
+    //find if destination exists
+    getDestinations = () => {
+        let user_id = this.props.id
+
+        fetch(`https://before-you-go.herokuapp.com/users/${user_id}`)
+        .then(response => response.json())
+        .then(response => this.addTrip(response))
+    }
+
+    //get user's destinations and post new one
+    addTrip = (response) => { 
+
+        let dests = []
+
+        if (response.destinations) {
+            dests = response.destinations.map(dest => dest.name)
+        }
+
+        if(!dests.includes(this.destinationSearch)){
+            fetch('https://before-you-go.herokuapp.com/destinations', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name: this.destinationSearch,    
+                    user_id: this.props.id
+                })
+            }).then(response => response.json())
+            .then( response => this.setState({ destination: response.name, destination_id: response.id }))    
+        } else {
+            this.setState({ message: "destination exists"})
+            let destination = response.destinations.find(dest => { return dest.name == this.destinationSearch })
+            this.setState({ destination: destination.name, destination_id: destination.id })
+        } 
+    }
+
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        let destination = `Novels set in ${this.destinationSearch}`
+        
+        fetch(encodeURI(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=categorymembers&cmtitle=Category:${destination}&cmlimit=18&origin=*`))
+        .then(response => response.json())
+        .then(data => {
+            let books = [];
+            data.query.categorymembers.forEach(book => { books.push(book.title)})
+            this.setState({ books: books, notFound: books.length })
+        })
+    }
+
+    render(){
+
+        return(
+            <div className="searchContainer">
+                <form onSubmit={this.handleSubmit} >
+                    <label>{this.state.destination}:</label><br/>
+                    <input type="text" placeholder="" name="destination" onChange={(e) => this.handleChange(e.target.value)} /><br/>
+                    <button className="buttonPage" onClick={this.getDestinations}>Create Trip</button>
+                    <button className="buttonPage">Search for Books</button>
+                </form>
+                
+                {
+                    this.state.message == "destination exists"
+                    ?
+                    <p>You already have a trip to this destination. Feel free to add some Books!</p>
+                    :
+                    <div></div>
+                }
+                { this.state.notFound == 0 ?
+                    <p>No Books were found for this destination.</p>
+                :
+                <BookDisplay bookTitles={this.state.books} destination_id={this.state.destination_id} />
+                }
+            </div>
+        )
+    }
+}
+
+export default Form;
