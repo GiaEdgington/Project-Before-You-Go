0ae83bd1ea5bfc34eb463b8edb4ebcbf2787f89c
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-10-30 22:04:52 -0400

    links updated

--- src/components/Book.js
+++ src/components/Book.js
@@ -37,12 +37,12 @@ class Book extends React.Component {
         });
     };
 
-    //book details
+    //for book details
     handleClick = () => {
         this.setState({ isHidden: !this.state.isHidden})
     };
 
-    //saves book
+    //saving book
     //find if destination already exists
     addBook = () => {
 
